5552b8b89de6cb599e7e176537002c27a753fa41
Youssef <youssef95elzein@gmail.com>
2024-05-14 22:19:27 -0700

    round two baby!

--- .config/nvim/lua/younix/theme.lua
+++ .config/nvim/lua/younix/theme.lua
@@ -1,12 +1,12 @@
 require('rose-pine').setup({
-    dark_variant = 'moon'
+    dark_variant = 'main'
 })
 vim.g.everforest_better_performance = 1
-vim.g.everforest_background = 'medium'
+vim.g.everforest_background = 'hard'
 
 vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_background = 'hard'
 
 vim.o.background = 'dark'
 
-vim.cmd('colorscheme gruvbox-material')
+vim.cmd('colorscheme rose-pine')
