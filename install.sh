db560eddbda6738fe5de021319be65aad825e1c0
Youssef <youssef95elzein@gmail.com>
2024-05-19 16:45:16 -0700

    no longer using aur and reorganized script

--- install.sh
+++ install.sh
@@ -1,32 +1,29 @@
-sudo pacman -Syu
-
-sudo pacman -S kitty zsh starship ttf-iosevka-nerd neovim ripgrep zathura zathura-pdf-poppler wl-clipboard ranger nvtop htop discord spotify-launcher obsidian jq nodejs npm python-pipx python-pillow
-# cmake extra-cmake-modules bluez bluedevil
-# AUR: dropbox expressvpn spotify spicetify-cli
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
-
-pipx install https://github.com/leovoel/BeautifulDiscord/archive/master.zip
-
+# Delete unnecessary folders
 sudo rm -rf ~/Documents ~/Music ~/Public ~/Videos ~/Templates ~/Pictures
 
+# Copy config files
 sudo rsync -a .config .local .zshrc ~/
+
+# Copy theming scripts
 sudo cp .config/theme-setter/set-theme .config/theme-setter/get-theme /usr/local/bin/
 
-sudo systemctl enable bluetooth
-#sudo systemctl enable expressvpn
+# Update and install packages
+sudo pacman -Syu && sudo pacman -S kitty starship ttf-iosevka-nerd neovim ripgrep zathura zathura-pdf-poppler wl-clipboard ranger nvtop htop discord spotify-launcher obsidian jq nodejs npm python-pipx python-pillow zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
 
-chsh -s $(which zsh)
+# Install Dropbox
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+# ~/.dropbox-dist/dropboxd
 
-# set discord theme
-# ~/.local/bin/beautifuldiscord --css ~/.config/beautdisc/global_theme.css
-
-# set spotify theme
-# sudo chmod a+wr /opt/spotify
-# sudo chmod a+wr /opt/spotify/Apps -R
+# Install Spicetify
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 # spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
 # spicetify config current_theme Comfy
 # spicetify config color_scheme dynamic
 # spicetify backup apply
+
+# Install Beautiful Discord
+pipx install https://github.com/leovoel/BeautifulDiscord/archive/master.zip
+# ~/.local/bin/beautifuldiscord --css ~/.config/beautifuldiscord.css
+
+# Make zsh default shell
+chsh -s $(which zsh)
