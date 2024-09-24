76dee0b6358c2ccbd067d501a5e367413d597168
Youssef <youssef95elzein@gmail.com>
2024-09-23 21:55:41 -0700

    Added docker config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo rsync -a .config .local .zshrc ~/
 sudo cp .config/theme-setter/set-theme .config/theme-setter/get-theme /usr/local/bin/
 
 # Update and install packages
-yay -Syu && yay -S vivaldi kitty starship ttf-jetbrains-mono ttf-jetbrains-mono-nerd code neovim ripgrep zathura zathura-pdf-poppler wl-clipboard ranger nvtop htop expressvpn dropbox discord spotify-launcher spicetify-cli obsidian jq nodejs npm python-pipx python-pillow zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
+yay -Syu && yay -S kitty starship ttf-jetbrains-mono ttf-jetbrains-mono-nerd code neovim ripgrep zathura zathura-pdf-poppler wl-clipboard ranger nvtop htop expressvpn dropbox discord spotify-launcher spicetify-cli obsidian jq python-pipx python-pillow docker docker-buildx zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
 
 # Install Spicetify
 # spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
@@ -19,6 +19,14 @@ yay -Syu && yay -S vivaldi kitty starship ttf-jetbrains-mono ttf-jetbrains-mono-
 # Install Beautiful Discord
 pipx install https://github.com/leovoel/BeautifulDiscord/archive/master.zip
 # ~/.local/bin/beautifuldiscord --css ~/.config/beautifuldiscord.css
+
+# Enable Bluetooth, Docker, and ExpressVPN
+sudo systemctl enable bluetooth
+sudo systemctl enable docker
+sudo systemctl enable expressvpn
+
+# Add user to Docker group
+sudo gpasswd -a $USER docker
 
 # Make zsh default shell
 chsh -s $(which zsh)
